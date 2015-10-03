8b1ac1a66545ccdada0a1dc3799f9fa02a873184
a100d100 <addor0guileness@gmail.com>
2015-10-03 10:41:02 +0000

    temp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,42 +2,43 @@ class UsersController < ApplicationController
   def show # 追加
    @user = User.find(params[:id])
   end
+  
   def new
     @user = User.new
   end
   
-    def create
+  def create
     @user = User.new(user_params)
     if @user.save
       redirect_to @user
     else
       render 'new'
     end
-    end
+  end
     
-    def edit
-      @user = User.find(params[:id])
-    end
+  def edit
+    @user = User.find(params[:id])
+  end
     
-    def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        redirect_to edit_user_path , notice: 'プロフィールを更新しました'
-      else
-        render 'edit'
-      end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path , notice: 'プロフィールを更新しました'
+    else
+      render 'edit'
     end
+  end
     
-    def followings
+  def followings
     @user = User.find(params[:followed_id])
     current_user.show(@user)
-  　end
+  end
     
-    def followers
+  def followers
     @user = User.find(params[:follower_id])
     current_user.show(@user)
-  　end
-
+  end
+    
   private
 
   def user_params
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001163321) do
+ActiveRecord::Schema.define(version: 20151002092115) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -35,15 +35,13 @@ ActiveRecord::Schema.define(version: 20151001163321) do
   add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.string   "area"
+    t.string   "age"
     t.string   "profile"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
