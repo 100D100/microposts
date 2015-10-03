b92ffcd8b615406d6a711ecac5fd09c33b131707
a100d100 <addor0guileness@gmail.com>
2015-10-03 16:03:41 +0000

    kadaiyonkaime

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,32 +2,32 @@ class UsersController < ApplicationController
   def show # 追加
    @user = User.find(params[:id])
   end
+  
   def new
     @user = User.new
   end
   
-    def create
+  def create
     @user = User.new(user_params)
     if @user.save
       redirect_to @user
     else
       render 'new'
     end
-    end
+  end
     
-    def edit
-      @user = User.find(params[:id])
-    end
+  def edit
+    @user = User.find(params[:id])
+  end
     
-    def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        redirect_to edit_user_path , notice: 'プロフィールを更新しました'
-      else
-        render 'edit'
-      end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path , notice: 'プロフィールを更新しました'
+    else
+      render 'edit'
     end
-    
+  end
 
   private
 
